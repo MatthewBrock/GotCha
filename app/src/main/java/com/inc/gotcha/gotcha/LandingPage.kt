b25f739ff2d438d6e2bdcc752627fc84aa2ab4f5
MatthewBrock <mbrock1997@gmail.com>
2018-12-14 10:28:05 -0500

    HCE demo working

--- app/src/main/java/com/inc/gotcha/gotcha/LandingPage.kt
+++ app/src/main/java/com/inc/gotcha/gotcha/LandingPage.kt
@@ -1,69 +1,12 @@
 package com.inc.gotcha.gotcha
 
-import android.content.Intent
-import android.nfc.NdefMessage
-import android.nfc.NdefRecord.createMime
-import android.nfc.NfcAdapter
-import android.nfc.NfcEvent
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class LandingPage : AppCompatActivity(), NfcAdapter.CreateNdefMessageCallback {
-
-    private var nfcAdapter: NfcAdapter? = null
-    private lateinit var textView: TextView
+class LandingPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.landing_page_activity)
-        textView = findViewById(R.id.test_message_preview)
-        textView.text = "no message yet"
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        if (nfcAdapter == null) {
-            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show()
-            finish()
-            return
-        }
-        nfcAdapter?.setNdefPushMessageCallback(this, this)
-    }
-
-    override fun createNdefMessage(event: NfcEvent): NdefMessage {
-        val text = "Beam me up, Android!\n\n" +
-                "Beam Time: " + System.currentTimeMillis()
-        return NdefMessage(
-                arrayOf(
-                        createMime("application/com.inc.gotcha.gotcha", text.toByteArray())
-                )
-                /**
-                 * The Android Application Record (AAR) is commented out. When a device
-                 * receives a push with an AAR in it, the application specified in the AAR
-                 * is guaranteed to run. The AAR overrides the tag dispatch system.
-                 * You can add it back in to guarantee that this
-                 * activity starts when receiving a beamed message. For now, this code
-                 * uses the tag dispatch system.
-                 *///,NdefRecord.createApplicationRecord("com.example.android.beam")
-        )
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        setIntent(intent)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
-            processIntent(intent)
-        }
-    }
-
-    private fun processIntent(intent: Intent) {
-        intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMsgs ->
-            (rawMsgs[0] as NdefMessage).apply {
-                // record[0] contains the MIME type, record[1] is the AAR, if present
-                textView.text = String(records[0].payload)
-            }
-        }
     }
 }
