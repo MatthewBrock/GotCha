bb221f8b2ab0a4aac7bfbeebdc683009341a5075
Christian Crimi <christian.crimi@kik.com>
2018-12-17 15:17:00 -0500

    Initializing contact list page

--- app/src/main/java/com/inc/gotcha/gotcha/ui/contactlistpage/ContactListFragment.kt
+++ app/src/main/java/com/inc/gotcha/gotcha/ui/contactlistpage/ContactListFragment.kt
@@ -9,6 +9,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.inc.gotcha.gotcha.R
 import com.inc.gotcha.gotcha.databinding.ContactListFragmentBinding
+//import android.support.v7.widget.LinearLayoutManager
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.inc.gotcha.gotcha.R.id.contactListFragment
+import com.inc.gotcha.gotcha.R.id.rv_animal_list
+import kotlinx.android.synthetic.main.contact_list_fragment.*
+import kotlinx.android.synthetic.main.contact_list_fragment.view.*
+
 
 class ContactListFragment : Fragment() {
 
@@ -21,9 +29,35 @@ class ContactListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         viewModel = ViewModelProviders.of(this).get(ContactListViewModel::class.java)
-        val binding: ContactListFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.profile_fragment, container, false)
+        val binding: ContactListFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.contact_list_fragment, container, false)
         binding.viewmodel = viewModel
         binding.setLifecycleOwner(this)
         return binding.root
+
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.contact_list_item)
+//
+//        // Creates a vertical Layout Manager
+//        rv_animal_list.linearmanager = LinearLayoutManager(this)
+//
+//        // Access the RecyclerView Adapter and load the data into it
+//        rv_animal_list.linearmanager = AnimalAdapter(animals, this)
+//
+//        val animals: ArrayList<String> = ArrayList()
+//        // Adds animals to the empty animals ArrayList
+//        fun addAnimals() {
+//            animals.add("dog")
+//            animals.add("cat")
+//            animals.add("owl")
+//            animals.add("cheetah")
+//            animals.add("raccoon")
+//            animals.add("bird")
+//            animals.add("snake")
+//            animals.add("lizard")
+//            animals.add("hamster")
+//            animals.add("bear")
+//            animals.add("lion")
+//        }
+//        addAnimals()
     }
 }
