0f0875850da4db1d248ed90bdeaace0e5ac8c13b
MatthewBrock <mbrock1997@gmail.com>
2018-12-17 15:42:36 -0500

    gradient added

--- app/src/main/java/com/inc/gotcha/gotcha/LandingPage.kt
+++ app/src/main/java/com/inc/gotcha/gotcha/LandingPage.kt
@@ -2,13 +2,17 @@ package com.inc.gotcha.gotcha
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+
 
 class LandingPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.landing_page_activity)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        setStatusBarGradiant()
     }
 
     override fun onResume() {
@@ -16,5 +20,14 @@ class LandingPage : AppCompatActivity() {
         val hceServiceIntent = Intent(this.applicationContext, HostCardEmulatorService::class.java)
         hceServiceIntent.putExtra("Data", "yolo swag bughati")
         startService(hceServiceIntent)// a bit of a hack to send data to this service, binding to it is hard
+    }
+
+    fun setStatusBarGradiant() {
+        val window = window
+        val background = resources.getDrawable(R.drawable.gradient_background)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = resources.getColor(R.color.status_bar_tint)
+        //window.navigationBarColor = activity.resources.getColor(android.R.color.transparent)
+        window.setBackgroundDrawable(background)
     }
 }
