4a78ccc6580aa4faea2d865689960ec1c16eb4f3
Michael Rokas <michaelrokas649@gmail.com>
2018-12-17 21:21:18 -0500

    added color to profile page, and ability to delete them

--- app/src/main/java/com/inc/gotcha/gotcha/ui/profilepage/ProfileViewModel.kt
+++ app/src/main/java/com/inc/gotcha/gotcha/ui/profilepage/ProfileViewModel.kt
@@ -2,16 +2,21 @@ package com.inc.gotcha.gotcha.ui.profilepage
 
 import android.content.SharedPreferences
 import android.content.res.Resources
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
 import com.inc.gotcha.gotcha.MediaElement
 import com.inc.gotcha.gotcha.ProfileData
-import com.inc.gotcha.gotcha.R
 
 class ProfileViewModel(val sharedPref: SharedPreferences?, val resources: Resources) : ViewModel(), IProfileViewModel {
     val PROFILE = "PROFILE"
 
+    override val buttonVisibility = MutableLiveData<Int>()
+
     private val fieldVMs = ArrayList<IProfileFieldViewModel>()
+
+    private var nextVMIndex = 5
 
     init {
         val profileDataString = sharedPref?.getString(PROFILE,"{}")
@@ -23,12 +28,16 @@ class ProfileViewModel(val sharedPref: SharedPreferences?, val resources: Resour
             mediaList.addAll(data.mediaList)
 
         for(i in 0..Math.min(5, mediaList.size) - 1) {
-            fieldVMs.add(ProfileFieldViewModel(mediaList[i]?.mediaHandle, resources){a, b -> save(a,b)})
+            fieldVMs.add(ProfileFieldViewModel(true, mediaList[i]?.mediaHandle, mediaList[i]?.mediaType,
+                    resources, {a, b -> save(a,b)}, {a, b -> delete(a,b)}))
         }
 
         while(fieldVMs.size < 5) {
-            fieldVMs.add(ProfileFieldViewModel(resources.getString(R.string.add_new_profile), resources){ a, b -> save(a,b)})
+            fieldVMs.add(ProfileFieldViewModel(false, "", "", resources, { a, b -> save(a,b)}, { a, b -> delete(a,b)}))
         }
+
+        nextVMIndex = mediaList.size
+        buttonVisibility.value = if(mediaList.size < 5) View.VISIBLE else View.GONE
     }
 
     override fun getFieldVMs(): ArrayList<IProfileFieldViewModel> = fieldVMs
@@ -51,5 +60,43 @@ class ProfileViewModel(val sharedPref: SharedPreferences?, val resources: Resour
         val editor = sharedPref?.edit()
         editor?.putString(PROFILE, dataToSave)
         editor?.apply()
+
+        if(nextVMIndex < 5)
+            buttonVisibility.value = View.VISIBLE
+    }
+
+    fun delete(mediaType: String?, mediaHandle: String?) {
+        println("$mediaType $mediaHandle")
+
+        val profileDataString = sharedPref?.getString(PROFILE,"{}")
+        val data = Gson().fromJson(profileDataString, ProfileData::class.java)
+
+        var tempList = ArrayList<MediaElement>()
+
+        if(data?.mediaList != null)
+            tempList.addAll(data.mediaList)
+
+        for(i in 0..tempList.size-1) {
+            if(tempList[i].mediaType.equals(mediaType?.toLowerCase()) && tempList[i].mediaHandle.equals(mediaHandle)) {
+                tempList.removeAt(i)
+                break
+            }
+        }
+
+        val dataToSave = Gson().toJson(ProfileData(tempList))
+
+        val editor = sharedPref?.edit()
+        editor?.putString(PROFILE, dataToSave)
+        editor?.apply()
+    }
+
+    override fun addHandle() {
+        if(nextVMIndex >= 5)
+            return
+
+        fieldVMs[nextVMIndex].enterCreation()
+        nextVMIndex++
+
+        buttonVisibility.value = View.GONE
     }
 }
