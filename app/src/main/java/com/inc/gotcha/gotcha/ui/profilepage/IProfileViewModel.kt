bf8a59933de1e1a191ba92a7eaba73207879e1b0
Michael Rokas <michaelrokas649@gmail.com>
2018-12-18 11:56:14 -0500

    added name field

--- app/src/main/java/com/inc/gotcha/gotcha/ui/profilepage/IProfileViewModel.kt
+++ app/src/main/java/com/inc/gotcha/gotcha/ui/profilepage/IProfileViewModel.kt
@@ -4,8 +4,13 @@ import androidx.lifecycle.MutableLiveData
 
 interface IProfileViewModel {
     val buttonVisibility: MutableLiveData<Int>
+    val showEdit: MutableLiveData<Int>
+    val showName: MutableLiveData<Int>
+    val name: MutableLiveData<String>
 
     fun getFieldVMs(): ArrayList<IProfileFieldViewModel>
     fun save(type: String?, mediaString: String?)
     fun addHandle()
+    fun editName()
+    fun saveName()
 }
